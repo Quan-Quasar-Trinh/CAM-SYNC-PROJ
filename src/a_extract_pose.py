1d268e44bf160f7893ea6d1647ac41b31188661b
Quan-Quasar-Trinh <quan.trinhquasar@hcmut.edu.vn>
2026-04-06 19:41:57 +0700

    play around pose extraction args

--- src/a_extract_pose.py
+++ src/a_extract_pose.py
@@ -7,17 +7,18 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 def extract_keypoints_from_video(video_path, output_npy="keypoints.npy"):
-    base_options = python.BaseOptions(model_asset_path='pose_landmarker_lite.task')
+    base_options = python.BaseOptions(model_asset_path='pose_landmarker_heavy.task')
     options = vision.PoseLandmarkerOptions(
         base_options=base_options,
         running_mode=vision.RunningMode.VIDEO,
         num_poses=1,
-        min_pose_detection_confidence=0.75,
-        min_pose_presence_confidence=0.75,
-        min_tracking_confidence=0.75,
+        min_pose_detection_confidence=0.6,
+        min_pose_presence_confidence=0.6,
+        min_tracking_confidence=0.6,
         output_segmentation_masks=False
     )
     
+    print(f"OPTION{options}")
     landmarker = vision.PoseLandmarker.create_from_options(options)
     
     cap = cv2.VideoCapture(video_path)
